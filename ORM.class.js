7e35933b5c3e7e6aa9e256b60c4787b50f91ea69
Fr13dr1ch99 <fr13dr1ch99@gmail.com>
2020-08-01 01:22:31 +0200

    Add ORM base

---
+++ ORM.class.js
@@ -0,0 +1,14 @@
+const Entity = require("./src/Entity.class.js");
+const Repository = require("./src/Repository.class.js");
+
+
+/**
+ * ORM - Representate the ORM Object
+ */
+class ORM {
+  constructor() {
+    
+  }
+}
+
+module.exports = {ORM, Entity, Repository};
